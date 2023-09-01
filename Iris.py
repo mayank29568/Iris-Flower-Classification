c0ab631c308bbb53fdf7fc71e96208054e75841c
mayank29568 <115179576+mayank29568@users.noreply.github.com>
2023-09-02 00:19:35 +0530

    Create Iris.py

---
+++ Iris.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from sklearn.preprocessing import LabelEncoder
+warnings.filterwarnings('ignore')
+df= pd.read_csv('/kaggle/input/iriscsv/Iris.csv')
+df.head()
+df= df.drop(columns=["Id"])
+df.head()
+# Display basic statistics about the data
+df.describe()
+# Display information about the datatype of each column and null values
+df.info()
+# Display the number of samples for each class
+df['Species'].value_counts()
+#Label encoding to convert class labels into numeric form
+
+le = LabelEncoder()
+df['Species'] = le.fit_transform(df['Species'])
+df['Species']
+# check for null values
+df.isnull().sum()
+# Plot histograms of each feature
+df['SepalLengthCm'].hist(color='red')
+df['SepalWidthCm'].hist(color='red')
+df['PetalLengthCm'].hist(color='red')
+df['PetalWidthCm'].hist(color='red')
+#Plotting the histogram of all features toghether
+df['SepalLengthCm'].hist()
+df['SepalWidthCm'].hist()
+df['PetalLengthCm'].hist()
+df['PetalWidthCm'].hist()
+# Plot scatterplots to visualize relationships between features
+colors = ['red', 'green', 'blue']
+species = [0, 1, 2]
+# Scatter plot for Sepal Length vs Sepal Width
+for i in range(3):
+    x = df[df['Species'] == species[i]]
+    plt.scatter(x['SepalLengthCm'], x['SepalWidthCm'], c=colors[i], label=species[i])
+plt.xlabel("Sepal Length")
+plt.ylabel("Sepal Width")
+plt.legend()
+# Scatter plot for Petal Length vs Petal Width 
+for i in range(3):
+    x = df[df['Species'] == species[i]]
+    plt.scatter(x['PetalLengthCm'], x['PetalWidthCm'], c = colors[i], label=species[i])
+plt.xlabel("Petal Length")
+plt.ylabel("Petal Width")
+plt.legend()
+# Scatter plot for Petal Length vs Sepal Length
+for i in range(3):
+    x = df[df['Species'] == species[i]]
+    plt.scatter(x['SepalLengthCm'], x['PetalLengthCm'], c = colors[i], label=species[i])
+plt.xlabel("Sepal Length")
+plt.ylabel("Petal Length")
+plt.legend()
+# Scatter plot for Sepal Width vs Petal Width
+for i in range(3):
+    x = df[df['Species'] == species[i]]
+    plt.scatter(x['SepalWidthCm'], x['PetalWidthCm'], c = colors[i], label=species[i])
+plt.xlabel("Sepal Width")
+plt.ylabel("Petal Width")
+plt.legend()
+# Compute the correlation matrix 
+df.corr()
+# display the correlation matrix using a heatmap
+corr = df.corr()
+fig, ax = plt.subplots(figsize=(5, 4))
+sns.heatmap(corr, annot=True, ax=ax, cmap='coolwarm')
+# Split the dataset into training and testing sets
+from sklearn.model_selection import train_test_split
+X = df.drop(columns=['Species'])
+Y = df['Species']
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.40)
+# Logistic Regression Model
+from sklearn.linear_model import LogisticRegression
+model1 = LogisticRegression()
+model1.fit(x_train, y_train)
+print("Accuracy (Logistic Regression): ", model1.score(x_test, y_test) * 100)
+# K-nearest Neighbours Model (KNN)
+from sklearn.neighbors import KNeighborsClassifier
+model2 = KNeighborsClassifier()
+model2.fit(x_train, y_train)
+print("Accuracy (KNN): ", model2.score(x_test, y_test) * 100)
+# Decision Tree Model
+from sklearn.tree import DecisionTreeClassifier
+model3 = DecisionTreeClassifier()
+model3.fit(x_train, y_train)
+print("Accuracy (Decision Tree): ", model3.score(x_test, y_test) * 100)
+from sklearn.metrics import confusion_matrix
+y_pred1 = model1.predict(x_test)
+y_pred2 = model2.predict(x_test)
+y_pred3 = model3.predict(x_test)
+conf_matrix1 = confusion_matrix(y_test, y_pred1)
+conf_matrix2 = confusion_matrix(y_test, y_pred2)
+conf_matrix3 = confusion_matrix(y_test, y_pred3)
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix1, annot=True, fmt='d', cmap='Blues', xticklabels=np.unique(Y), yticklabels=np.unique(Y))
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix of Logistic Regression')
+plt.show()
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix2, annot=True, fmt='d', cmap='Blues', xticklabels=np.unique(Y), yticklabels=np.unique(Y))
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix of KNN')
+plt.show()
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_matrix3, annot=True, fmt='d', cmap='Blues', xticklabels=np.unique(Y), yticklabels=np.unique(Y))
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix of Decision Tree')
+plt.show()
+
+                         
